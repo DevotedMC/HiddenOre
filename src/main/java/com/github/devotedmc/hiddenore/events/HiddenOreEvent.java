4eac367e220e0414e28ab214630d145bbf5b2037
Daniel Boston <programmerdan@gmail.com>
2016-02-27 13:55:10 -0500

    Quick finish up event

--- src/main/java/com/github/devotedmc/hiddenore/events/HiddenOreEvent.java
+++ src/main/java/com/github/devotedmc/hiddenore/events/HiddenOreEvent.java
@@ -1,8 +1,14 @@
 package com.github.devotedmc.hiddenore.events;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
 
 public class HiddenOreEvent extends Event implements Cancellable {
 	private static final HandlerList handlers = new HandlerList();
@@ -12,12 +18,10 @@ public class HiddenOreEvent extends Event implements Cancellable {
 	private List<ItemStack> drops;
 	
 	public HiddenOreEvent(final Player player, final Location dropLocation, List<ItemStack> drops) {
-		
-	}
-
-	public HiddenOreEvent(boolean isAsync) {
-		super(isAsync);
-		// TODO Auto-generated constructor stub
+		super(false);
+		this.player = player;
+		this.dropLocation = dropLocation;
+		this.drops = new ArrayList<ItemStack>(drops);
 	}
 
 	private boolean cancel = false;
@@ -40,6 +44,22 @@ public class HiddenOreEvent extends Event implements Cancellable {
 	public static HandlerList getHandlerList() {
 		return handlers;
 	}
+	
+	public Player getPlayer() {
+		return player;
 	}
-
+	
+	public Location getDropLocation() {
+		return dropLocation;
+	}
+	
+	public List<ItemStack> getDrops() {
+		return drops;
+	}
+	
+	public void setDrops(List<ItemStack> drops) {
+		if (drops == this.drops) return;
+		this.drops.clear();
+		this.drops.addAll(drops);
+	}
 }
