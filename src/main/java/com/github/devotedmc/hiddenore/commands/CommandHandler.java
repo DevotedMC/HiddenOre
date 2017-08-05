44a4f11ecd2467a25d6cc60ade7607bb5291fa6e
ProgrammerDan <programmerdan@gmail.com>
2017-08-05 01:12:32 -0400

    Adding a command to generate all drops as items in world, for debug purposes

--- src/main/java/com/github/devotedmc/hiddenore/commands/CommandHandler.java
+++ src/main/java/com/github/devotedmc/hiddenore/commands/CommandHandler.java
@@ -1,11 +1,18 @@
 package com.github.devotedmc.hiddenore.commands;
 
+import java.util.Map;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 
+import com.github.devotedmc.hiddenore.BlockConfig;
 import com.github.devotedmc.hiddenore.Config;
+import com.github.devotedmc.hiddenore.DropConfig;
+import com.github.devotedmc.hiddenore.DropItemConfig;
 import com.github.devotedmc.hiddenore.HiddenOre;
 
 public class CommandHandler implements CommandExecutor {
@@ -25,10 +32,50 @@ public class CommandHandler implements CommandExecutor {
 						sender.sendMessage("HiddenOre debug mode now " + (Config.isDebug ? "on" : "off"));
 						return true;
 					}
-				} else if (args.length >= 1) {
+				}
+				
+				if (args.length >= 1) {
 					if ("save".equals(args[0])) {
 						plugin.getTracking().liveSave();
 						sender.sendMessage("HiddenOre tracking forced live save scheduled");
+						return true;
+					} else if ("generate".equals(args[0])) {
+						if (!(sender instanceof Player)) {
+							sender.sendMessage("Cannot be run as console");
+						}
+						Player player = (Player) sender;
+						double mult = 1;
+						try {
+							mult = Integer.parseInt(args[1]);
+						} catch (Exception e) {mult = 1;}
+						final double vmult = mult;
+						
+						sender.sendMessage("Generating all drops, this could cause lag");
+						
+						Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+							@Override
+							public void run() {
+								long delay = 0l;
+								for (String blockConf : Config.instance.blockConfigs.keySet()) {
+									for (BlockConfig block : Config.instance.blockConfigs.get(blockConf)) {
+										for (String dropConf : block.getDrops()) {
+											DropConfig drop = block.getDropConfig(dropConf);
+											for (DropItemConfig item : drop.drops) {
+												Bukkit.getScheduler().runTaskLater(plugin,  new Runnable() {
+													@Override
+													public void run() {
+														sender.sendMessage(String.format("Block: %s, drop: %s", blockConf, dropConf));
+														Item dropped = player.getWorld().dropItem(player.getLocation().add(0, 1.0, 0), item.render(vmult));
+														dropped.setPickupDelay(20);
+													}
+												}, delay++);
+											}
+										}
+									}
+								}
+							}
+						});
+						
 						return true;
 					}
 				} else {
