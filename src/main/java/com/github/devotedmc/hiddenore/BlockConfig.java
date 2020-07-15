97235167700587ccf66e4dd9bc0c6dcf39b38dfb
Nuclear-Crow <68324267+Nuclear-Crow@users.noreply.github.com>
2020-07-15 22:54:24 +0300

    brackets

--- src/main/java/com/github/devotedmc/hiddenore/BlockConfig.java
+++ src/main/java/com/github/devotedmc/hiddenore/BlockConfig.java
@@ -114,7 +114,9 @@ public class BlockConfig {
 									Double.toString(tc == null ? 1.0 : tc.getDropChanceModifier()),
 									localChance, (tc == null ? null : tc.toString())});*/
 				}
-				if(localChance < 0)continue;
+				if(localChance < 0){
+					continue;
+				}
 				
 				if (dice >= cumChance && dice < cumChance + localChance) {
 					return dce.getKey();
